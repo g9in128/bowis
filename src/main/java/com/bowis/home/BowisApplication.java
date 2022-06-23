a72d2b9370b36a5976d79b4c001654bca99ab312
g9in128 <g9in128@naver.com>
2022-06-24 07:35:32 +0900

    first commit

---
+++ src/main/java/com/bowis/home/BowisApplication.java
@@ -0,0 +1,13 @@
+package com.bowis.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bowis/home/BowisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowis.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
